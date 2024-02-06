42ea5418ec773e498c1c567dc798af25287ff4a2
Scheffel333 <118644309+Scheffel333@users.noreply.github.com>
2024-02-06 08:54:17 +0530

    added create for income records

--- frontend/src/pages/finance_pages/income_records/AddNewIncomeRecord.js
+++ frontend/src/pages/finance_pages/income_records/AddNewIncomeRecord.js
@@ -1,12 +1,54 @@
-import React from "react";
-
+import React, { useState } from 'react';
+import axios from 'axios';
 import SideBar from "../../../components/SideBar";
 import Navbar from "../../../components/Navbar";
-import TestForm from "../../../components/finances/TestForm";
+
 import FinanceNavigation from "../../../components/finances/FinanceNavigation.js";
-import IncomeRecordsList from "../../../components/finances/finance_income/IncomeRecordsList";
+
+import {useNavigate} from "react-router-dom";
+import { useSnackbar } from 'notistack';
 
 export default function AddNewIncomeRecord() {
+    const [first_name, setFirstName] = useState('');
+    const [last_name, setLastName] = useState('');
+    const [uemail, setUemail] = useState('');
+    const [country, setCountry] = useState('');
+    const [street_address, setStreetAddress] = useState('');
+    const [city, setCity] = useState('');
+    const [region, setRegion] = useState('');
+    const [postal_code, setPostalCode] = useState('');
+    const [loading, setLoading] = useState(false);
+    const navigate = useNavigate();
+    const { enqueueSnackbar } = useSnackbar();
+
+
+    const handleSaveIncomeRecord = () => {
+    const data = {
+        first_name,
+        last_name,
+        uemail,
+        country,
+        street_address,
+        city,
+        region,
+        postal_code,
+    };
+    setLoading(true);
+    axios
+        .post('http://localhost:5555/financeincome', data)
+        .then(() => {
+            setLoading(false);
+            enqueueSnackbar('Record Created successfully', { variant: 'success' });
+            navigate('/');
+        })
+        .catch((error) => {
+            setLoading(false);
+            // alert('An error happened. Please Check console');
+            enqueueSnackbar('Error', { variant: 'error' });
+            console.log(error);
+        });
+};
+
     return (
         <div className="">
             <div className="border-b sticky top-0 z-10">
@@ -20,7 +62,173 @@ export default function AddNewIncomeRecord() {
 
                     <div className="w-full col-span-5 flex flex-col ">
                         <FinanceNavigation/>
-                        <TestForm/>
+                        <form className=" flex-col flex items-center justify-center">
+                            <div className="space-y-12 px-0 py-16 w-6/12 ">
+                                <div className="border-b border-gray-900/10 pb-12">
+                                    <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+                                        <div className="sm:col-span-3">
+                                            <label htmlFor="first-name"
+                                                   className="block text-sm font-medium leading-6 text-gray-900">
+                                                First name
+                                            </label>
+                                            <div className="mt-2">
+                                                <input
+                                                    type="text"
+                                                    name="first-name"
+                                                    value={first_name}
+                                                    onChange={(e) => setFirstName(e.target.value)}
+                                                    id="first-name"
+                                                    autoComplete="given-name"
+                                                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                                                />
+                                            </div>
+                                        </div>
+
+                                        <div className="sm:col-span-3">
+                                            <label htmlFor="last-name"
+                                                   className="block text-sm font-medium leading-6 text-gray-900">
+                                                Last name
+                                            </label>
+                                            <div className="mt-2">
+                                                <input
+                                                    type="text"
+                                                    name="last-name"
+                                                    value={last_name}
+                                                    onChange={(e) => setLastName(e.target.value)}
+                                                    id="last-name"
+                                                    autoComplete="family-name"
+                                                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                                                />
+                                            </div>
+                                        </div>
+
+                                        <div className="sm:col-span-4">
+                                            <label htmlFor="email"
+                                                   className="block text-sm font-medium leading-6 text-gray-900">
+                                                Email address
+                                            </label>
+                                            <div className="mt-2">
+                                                <input
+                                                    id="email"
+                                                    name="email"
+                                                    value={uemail}
+                                                    onChange={(e) => setUemail(e.target.value)}
+                                                    type="email"
+                                                    autoComplete="email"
+                                                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                                                />
+                                            </div>
+                                        </div>
+
+                                        <div className="sm:col-span-3">
+                                            <label htmlFor="country"
+                                                   className="block text-sm font-medium leading-6 text-gray-900">
+                                                Country
+                                            </label>
+                                            <div className="mt-2">
+                                                <select
+                                                    id="country"
+                                                    name="country"
+                                                    value={country}
+                                                    onChange={(e) => setCountry(e.target.value)}
+                                                    autoComplete="country-name"
+                                                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:max-w-xs sm:text-sm sm:leading-6"
+                                                >
+                                                    <option>United States</option>
+                                                    <option>Sri Lanka</option>
+                                                </select>
+                                            </div>
+                                        </div>
+
+                                        <div className="col-span-full">
+                                            <label htmlFor="street-address"
+                                                   className="block text-sm font-medium leading-6 text-gray-900">
+                                                Street address
+                                            </label>
+                                            <div className="mt-2">
+                                                <input
+                                                    type="text"
+                                                    name="street-address"
+                                                    value={street_address}
+                                                    onChange={(e) => setStreetAddress(e.target.value)}
+                                                    id="street-address"
+                                                    autoComplete="street-address"
+                                                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                                                />
+                                            </div>
+                                        </div>
+
+                                        <div className="sm:col-span-2 sm:col-start-1">
+                                            <label htmlFor="city"
+                                                   className="block text-sm font-medium leading-6 text-gray-900">
+                                                City
+                                            </label>
+                                            <div className="mt-2">
+                                                <input
+                                                    type="text"
+                                                    name="city"
+                                                    value={city}
+                                                    onChange={(e) => setCity(e.target.value)}
+                                                    id="city"
+                                                    autoComplete="address-level2"
+                                                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                                                />
+                                            </div>
+                                        </div>
+
+                                        <div className="sm:col-span-2">
+                                            <label htmlFor="region"
+                                                   className="block text-sm font-medium leading-6 text-gray-900">
+                                                State / Province
+                                            </label>
+                                            <div className="mt-2">
+                                                <input
+                                                    type="text"
+                                                    name="region"
+                                                    value={region}
+                                                    onChange={(e) => setRegion(e.target.value)}
+                                                    id="region"
+                                                    autoComplete="address-level1"
+                                                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                                                />
+                                            </div>
+                                        </div>
+
+                                        <div className="sm:col-span-2">
+                                            <label htmlFor="postal-code"
+                                                   className="block text-sm font-medium leading-6 text-gray-900">
+                                                ZIP / Postal code
+                                            </label>
+                                            <div className="mt-2">
+                                                <input
+                                                    type="number"
+                                                    name="postal-code"
+                                                    value={postal_code}
+                                                    onChange={(e) => setPostalCode(e.target.value)}
+                                                    id="postal-code"
+                                                    autoComplete="postal-code"
+                                                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                                                />
+                                            </div>
+                                        </div>
+                                    </div>
+
+
+                                </div>
+                                <div className="mt-6 flex items-center justify-end gap-x-6">
+                                    <button type="button" className="text-sm font-semibold leading-6 text-gray-900">
+                                        Cancel
+                                    </button>
+                                    <button
+                                        onClick={handleSaveIncomeRecord}
+                                        className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                                    >
+                                        Save
+                                    </button>
+                                </div>
+
+                            </div>
+                        </form>
                     </div>
                 </div>
             </div>
