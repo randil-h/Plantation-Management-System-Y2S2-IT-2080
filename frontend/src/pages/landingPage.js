2c143d91b94646eae3eb5467ee59452190a2debf
Scheffel333 <118644309+Scheffel333@users.noreply.github.com>
2024-02-06 15:36:53 +0530

    added breadcrumbs and updated finance

--- frontend/src/pages/landingPage.js
+++ frontend/src/pages/landingPage.js
@@ -3,7 +3,7 @@ import Navbar from "../components/utility/Navbar";
 import WelcomeBanner from "../components/landingPage/Welcome_banner"
 import FunctionBar from "../components/landingPage/Function_bar";
 import SubscribeNewsletter from "../components/landingPage/subscribe_newsletter";
-import LandingPageBanner from "../components/LandingPageBanner";
+import LandingPageBanner from "../components/landingPage/LandingPageBanner";
 import Footer from "../components/utility/Footer";
 
 export default function LandingPage() {
