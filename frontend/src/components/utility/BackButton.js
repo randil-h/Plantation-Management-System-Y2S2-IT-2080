29a83f347036edaf6dbb85e378787c0de88d72e6
Scheffel333 <118644309+Scheffel333@users.noreply.github.com>
2024-02-07 18:06:15 +0530

    added backbutton and changed breadcrumbs

--- frontend/src/components/utility/BackButton.js
+++ frontend/src/components/utility/BackButton.js
@@ -11,7 +11,7 @@ const BackButton = () => {
     return (
         <div className='flex pl-8 pt-4'>
             <button onClick={goBack}
-               className="font-medium text-blue-600  hover:underline">
+               className="font-medium text-blue-600 ">
                 <ArrowUturnLeftIcon
                     className="h-6 w-6 flex-none bg-gray-300 p-1 rounded-full text-gray-800 hover:bg-gray-500"
                     aria-hidden="true"/>
--- frontend/src/pages/finance_pages/transactions/ViewTransactionDetails.js
+++ frontend/src/pages/finance_pages/transactions/ViewTransactionDetails.js
@@ -36,7 +36,7 @@ export default function ViewTransactionDetails() {
 
                         <div className="px-8 py-8">
                             <div className="px-4 sm:px-0">
-                                <h3 className="text-base font-semibold leading-7 text-gray-900">Transaction Details</h3>
+                                <h3 className="text-base font-semibold leading-7 text-gray-900">Transaction Detail</h3>
                                 <p className="mt-1 max-w-2xl text-sm leading-6 text-gray-500">Personal details and
                                     application.</p>
                             </div>
