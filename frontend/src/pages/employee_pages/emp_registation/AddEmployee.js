032d1c1a75cb01c5fd61fe309843318d9fb348eb
Yasasvi31 <lightfury200231@gmail.com>
2024-02-11 11:03:46 +0530

    emp register form

--- frontend/src/pages/employee_pages/emp_registation/AddEmployee.js
+++ frontend/src/pages/employee_pages/emp_registation/AddEmployee.js
@@ -2,22 +2,31 @@ import Navbar from "../../../components/utility/Navbar";
 import SideBar from "../../../components/SideBar";
 import React from "react";
 import EmpForm from "../../../components/Employee/Employee_register/EmpForm";
+import EmployeeNavbar from "../../../components/Employee/EmployeeNavbar";
+
 
 export default function AddEmployee(){
     return (
 
-        <div className="">
+        <div className="flex-col">
+            {/* Navbar */}
             <div className="border-b sticky top-0 z-10">
                 <Navbar/>
             </div>
-            <div className="flex">
+            <div className="">
                 {/* Fixed Sidebar */}
-                <div className="fixed h-full">
-                    <SideBar/>
+                <div className="grid sm:grid-cols-6 ">
+                    <div className="col-span-1 sticky left-0 top-0">
+                        <SideBar/>
+                    </div>
+                    <div>
+                        <EmployeeNavbar/>
+                        <EmpForm/>
+                    </div>
                 </div>
+
             </div>
 
-            <EmpForm/>
         </div>
     );
 };
