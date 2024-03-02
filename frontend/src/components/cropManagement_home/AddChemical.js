d611d0be6aa3a7454aeb671a3e900b1e4c827258
randil-h <51442488+randil-h@users.noreply.github.com>
2024-03-02 19:06:27 +0530

    added backend code of 2 crop management functions

--- frontend/src/components/cropManagement_home/AddChemical.js
+++ frontend/src/components/cropManagement_home/AddChemical.js
@@ -5,7 +5,7 @@ export default function AddChemical() {
         <div className="relative w-full flex justify-center items-start">
             <div className="ml-2/3 mt-8">
                 <h1 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl absolute inset-x-0 top-0 flex justify-cente pb-16 mt-4">
-                    Add Fertiliser
+                    Add Chemicals
                 </h1>
             </div>
             <form className="max-w-md ml-1/3 mt-16 p-4 bg-gray-200 rounded-lg">
@@ -29,7 +29,7 @@ export default function AddChemical() {
                 />
 
                 <label className="block mb-4">
-                    Fertiliser Name:
+                    Chemical Name:
                     <input
                         type="text"
                         className="w-full p-2 border rounded transition duration-300 focus:outline-none focus:border-blue-500 hover:shadow-md"
