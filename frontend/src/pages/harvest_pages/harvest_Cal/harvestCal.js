05394c5d327b472dc75c8dabdf70da3d925a0767
it22093332 <it22093332@my.sliit.lk>
2024-03-03 10:02:04 +0530

    harvest updates2

--- frontend/src/pages/harvest_pages/harvest_Cal/harvestCal.js
+++ frontend/src/pages/harvest_pages/harvest_Cal/harvestCal.js
@@ -3,6 +3,7 @@ import {Link} from 'react-router-dom'
 
 import Navbar from "../../../components/utility/Navbar";
 import SideBar from "../../../components/SideBar";
+import HarvestNavigation from "../../../components/harvest/harvestNavigation";
 
 
 
@@ -11,12 +12,18 @@ export default function harvestCal() {
         <div className="flex-col">
             {/* Navbar */}
             <div className="border-b sticky top-0 z-10">
-                <Navbar />
+                <Navbar/>
             </div>
-            <div className="flex">
-                {/* Fixed Sidebar */}
-                <div className="fixed h-full">
-                    <SideBar />
+            <div className="">
+                <div className="grid sm:grid-cols-6 ">
+
+                    <div className="col-span-1 sticky left-0 top-0">
+                        <SideBar/>
+                    </div>
+
+                    <div className="w-full col-span-5 flex flex-col ">
+                        <HarvestNavigation/>
+                    </div>
                 </div>
 
             </div>
--- frontend/src/pages/harvest_pages/harvest_records/harvestRecords.js
+++ frontend/src/pages/harvest_pages/harvest_records/harvestRecords.js
@@ -3,18 +3,25 @@ import {Link} from 'react-router-dom'
 
 import Navbar from "../../../components/utility/Navbar";
 import SideBar from "../../../components/SideBar";
+import HarvestNavigation from "../../../components/harvest/harvestNavigation";
 
 export default function harvestRec() {
     return (
         <div className="flex-col">
             {/* Navbar */}
             <div className="border-b sticky top-0 z-10">
-                <Navbar />
+                <Navbar/>
             </div>
-            <div className="flex">
-                {/* Fixed Sidebar */}
-                <div className="fixed h-full">
-                    <SideBar />
+            <div className="">
+                <div className="grid sm:grid-cols-6 ">
+
+                    <div className="col-span-1 sticky left-0 top-0">
+                        <SideBar/>
+                    </div>
+
+                    <div className="w-full col-span-5 flex flex-col ">
+                        <HarvestNavigation/>
+                    </div>
                 </div>
 
             </div>
