3546a5affaff0a4f08d9c20a642e741f82b1a6d7
susadi <susadisandanima@gmail.com>
2024-03-04 19:47:58 +0530

    Added chemical list backend

--- backend/index.js
+++ backend/index.js
@@ -16,6 +16,7 @@ import ChemicalRoute from "./routes/Crop Routes/ChemicalRoute.js"
 
 import EqMaintainroute from "./routes/Inventory Routes/EqMaintainroute.js";
 import AddSeedRoute from "./routes/Inventory Routes/AddSeedRoute.js";
+import AddChemicalRoute from "./routes/Inventory Routes/AddChemicalRoute.js";
 
 
 const app = express();
@@ -51,7 +52,7 @@ app.use('/chemicals', ChemicalRoute);
 
 app.use('/inventoryrecords', EqMaintainroute);
 app.use('/seedRecords', AddSeedRoute);
-
+app.use('/chemicalRecords', AddChemicalRoute);
 mongoose
     .connect(mongoDBURL)
     .then(() => {
