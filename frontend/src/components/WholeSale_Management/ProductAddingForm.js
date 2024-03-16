8751b05c100f4067a662792b668a7be4e43fe5a2
Vinuk Lakvindu <it22055026@my.sliit.lk>
2024-03-17 01:10:45 +0530

    change things related to backend

--- frontend/src/components/WholeSale_Management/ProductAddingForm.js
+++ frontend/src/components/WholeSale_Management/ProductAddingForm.js
@@ -1,92 +1,115 @@
-import React from "react";
+import React, {useState} from "react";
+import axios from "axios";
 
 export default function ProductAddingForm(){
+    const [formData, setFormData] = useState({
+        productID: '',
+        productName: '',
+        productDescription: '',
+        productQuantity: '',
+        productPrice: ''
+    });
+
+    const handleChange = (e) => {
+        setFormData({...formData, [e.target.name]: e.target.value});
+    };
+
+    const handlesubmit = async (e) => {
+        e.preventDefault();
+
+        try {
+            const response = await axios.post('', formData);
+            console.log(response.data);
+            setFormData({
+                productID: '',
+                productName: '',
+                productDescription: '',
+                productQuantity: '',
+                productPrice: ''
+            });
+        }catch (error) {
+            console.log(error.message);
+        }
+    }
     return (
         <div>
             <form>
                 <div className="mx-auto max-w-7xl px-6 lg:px-8">
                     <div className="space-y-12">
                         <div className="border-b border-gray-900/10 pb-12">
-                            {/*<h2 className="text-base font-semibold leading-7 text-gray-900">Profile</h2>*/}
-                            {/*<p className="mt-1 text-sm leading-6 text-gray-600">This information will be*/}
-                            {/*    displayed publicly so be careful what you share.</p>*/}
-
-                            {/*<div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">*/}
-                            {/*<div className="sm:col-span-4">*/}
-                            {/*    /!*<label htmlFor="username"*!/*/}
-                            {/*    /!*       className="block text-sm font-medium leading-6 text-gray-900">Username</label>*!/*/}
-                            {/*    <div className="mt-2">*/}
-                            {/*        <div*/}
-                            {/*            className="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">*/}
-                            {/*            /!*<span*!/*/}
-                            {/*            /!*    className="flex select-none items-center pl-3 text-gray-500 sm:text-sm">workcation.com/</span>*!/*/}
-                            {/*            /!*<input type="text" name="username" id="username"*!/*/}
-                            {/*            /!*       autoComplete="username"*!/*/}
-                            {/*            /!*       className="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"*!/*/}
-                            {/*            /!*       placeholder="janesmith"/>*!/*/}
-                            {/*        </div>*/}
-                            {/*    </div>*/}
-                            {/*</div>*/}
-
-
-                            {/*</div>*/}
-                        </div>
-
-                        <div className="border-b border-gray-900/10 pb-12">
                             <h2 className="text-base font-semibold leading-7 text-gray-900">Add a Product</h2>
-                            {/*<p className="mt-1 text-sm leading-6 text-gray-600">Use a permanent address*/}
-                            {/*    where you can receive mail.</p>*/}
-
+                        </div>
+                    </div>
+                </div>
+                <div className="mx-auto max-w-7xl px-6 lg:px-8">
+                <div className="space-y-12">
+                        <div className="border-b border-gray-900/10 pb-12">
                             <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
                                 <div className="sm:col-span-3">
-                                    <label htmlFor="first-name"
+                                    <label
                                            className="block text-sm font-medium leading-6 text-gray-900">Product
                                         ID</label>
                                     <div className="mt-2">
-                                        <input type="text" name="first-name" id="first-name"
-                                               autoComplete="given-name"
+                                        <input type="text"
+                                               name="productID"
+                                               id="productID"
+                                               onChange={handleChange}
+                                               value={formData.productID}
                                                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"/>
                                     </div>
                                 </div>
 
                                 <div className="sm:col-span-3">
-                                    <label htmlFor="last-name"
+                                    <label
                                            className="block text-sm font-medium leading-6 text-gray-900">Product
                                         Name</label>
                                     <div className="mt-2">
-                                        <input type="text" name="last-name" id="last-name"
-                                               autoComplete="family-name"
+                                        <input type="text"
+                                               name="productName"
+                                               id="productName"
+                                               onChange={handleChange}
+                                               value={formData.productName}
                                                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"/>
                                     </div>
                                 </div>
 
                                 <div className="col-span-full">
-                                    <label htmlFor="about"
+                                    <label
                                            className="block text-sm font-medium leading-6 text-gray-900">Description</label>
                                     <div className="mt-2">
-                                            <textarea id="about" name="about" rows="3"
+                                            <textarea id="productDescription"
+                                                      name="productDescription"
+                                                      onChange={handleChange}
+                                                      value={formData.productDescription}
+                                                      rows="3"
                                                       className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"></textarea>
                                     </div>
 
                                 </div>
 
                                 <div className="sm:col-span-3">
-                                    <label htmlFor="first-name"
+                                    <label
                                            className="block text-sm font-medium leading-6 text-gray-900">Quantity</label>
                                     <div className="mt-2">
-                                        <input type="text" name="first-name" id="first-name"
-                                               autoComplete="given-name"
+                                        <input type="text"
+                                               name="productQuantity"
+                                               id="productQuantity"
+                                               onChange={handleChange}
+                                               value={formData.productQuantity}
                                                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"/>
                                     </div>
                                 </div>
 
                                 <div className="sm:col-span-3">
-                                    <label htmlFor="last-name"
+                                    <label
                                            className="block text-sm font-medium leading-6 text-gray-900">Price Per
                                         1Kg</label>
                                     <div className="mt-2">
-                                        <input type="text" name="last-name" id="last-name"
-                                               autoComplete="family-name"
+                                        <input type="text"
+                                               name="productPrice"
+                                               id="productPrice"
+                                               onChange={handleChange}
+                                               value={formData.productPrice}
                                                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"/>
                                     </div>
                                 </div>
@@ -95,9 +118,6 @@ export default function ProductAddingForm(){
                     </div>
 
                     <div className="mt-6 flex items-center justify-end gap-x-6">
-                        <button type="button"
-                                className="text-sm font-semibold leading-6 text-gray-900">Cancel
-                        </button>
                         <button type="submit"
                                 className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Save
                         </button>
