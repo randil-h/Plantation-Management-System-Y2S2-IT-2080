cc4d5bd0929980c4f0873eccbb739765f81e1a35
Yasasvi31 <lightfury200231@gmail.com>
2024-03-17 11:12:43 +0530

    update emplist.js

--- frontend/src/components/Employee/Employee_register/EmployeeList.js
+++ frontend/src/components/Employee/Employee_register/EmployeeList.js
@@ -6,6 +6,7 @@ import {
     TrashIcon,
     InformationCircleIcon
 } from '@heroicons/react/24/outline'
+import {Link} from "react-router-dom";
 
 const EmployeeList = () => {
 
@@ -162,18 +163,18 @@ const EmployeeList = () => {
                                     </a>
                                 </td>
                                 <td className=" py-4 text-right">
-                                    <a href= "/employees/registration/editEmployee" className="font-medium text-blue-600 hover:underline">
+                                    <Link to={`/employees/registration/editEmployee/${record._id}`}
+                                          className="font-medium text-blue-600 hover:underline">
                                         <PencilSquareIcon
                                             className="h-6 w-6 flex-none bg-blue-200 p-1 rounded-full text-gray-800 hover:bg-blue-500"
                                             aria-hidden="true"/>
-                                    </a>
+                                    </Link>
                                 </td>
-                                <td className=" py-4 text-right">
 
+                                <td className=" py-4 text-right">
                                     <button
                                         className="flex items-center"
                                         onClick={() => handleDelete(record._id)}
-
                                     >
                                         <TrashIcon
                                             className="h-6 w-6 flex-none bg-red-200 p-1 rounded-full text-gray-800 hover:bg-red-500"
