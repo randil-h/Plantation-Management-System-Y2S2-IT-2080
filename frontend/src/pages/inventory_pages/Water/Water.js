d694b8829debce6c30a32d1ab83d0a5ccb545d99
susadi <susadisandanima@gmail.com>
2024-03-17 18:45:25 +0530

    added notification to water page

--- frontend/src/pages/inventory_pages/Water/Water.js
+++ frontend/src/pages/inventory_pages/Water/Water.js
@@ -1,42 +1,41 @@
-
 import SideBar from "../../../components/SideBar";
-
 import React from "react";
-
 import Navbar from "../../../components/utility/Navbar";
 import WaterTank from "../../../components/Inventory/Water/WaterTank";
 import InventoryNavbar from "../../../components/Inventory/InventoryNavbar";
 import BackButton from "../../../components/utility/BackButton";
 import Breadcrumb from "../../../components/utility/Breadcrumbs";
-
+import { SnackbarProvider } from 'notistack'; // Import SnackbarProvider
 
 export default function Water() {
-
     const breadcrumbItems = [
         { name: 'Water', href: '/inventory/water' },
     ];
-    return (
-        <div className="Equipment">
-            <div className="border-b sticky top-0 z-10">
-                <Navbar/>
-            </div>
-            <div className="">
-                {/* Fixed Sidebar */}
-                <div className="grid sm:grid-cols-6 ">
-                    <div className="col-span-1 sticky left-0 top-0">
-                        <SideBar/>
-                    </div>
 
-                    <div className="w-full col-span-5 flex flex-col ">
-                        <InventoryNavbar/>
-                        <div className="flex flex-row ">
-                            <BackButton/>
-                            <Breadcrumb items={breadcrumbItems}/>
+    return (
+        <SnackbarProvider maxSnack={3}> {/* Wrap your component tree with SnackbarProvider */}
+            <div className="Equipment">
+                <div className="border-b sticky top-0 z-10">
+                    <Navbar/>
+                </div>
+                <div className="">
+                    {/* Fixed Sidebar */}
+                    <div className="grid sm:grid-cols-6 ">
+                        <div className="col-span-1 sticky left-0 top-0">
+                            <SideBar/>
                         </div>
-                        <WaterTank/>
+
+                        <div className="w-full col-span-5 flex flex-col ">
+                            <InventoryNavbar/>
+                            <div className="flex flex-row ">
+                                <BackButton/>
+                                <Breadcrumb items={breadcrumbItems}/>
+                            </div>
+                            <WaterTank/>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
+        </SnackbarProvider>
     );
 }
