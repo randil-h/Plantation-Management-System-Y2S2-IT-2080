e98d042190da36f7ae59c61757f7e782b44abf54
it22093332 <it22093332@my.sliit.lk>
2024-03-17 17:23:36 +0530

    harvest backend updates

--- backend/routes/Harvest Routes/RecordRoute.js
+++ backend/routes/Harvest Routes/RecordRoute.js
@@ -1,6 +1,7 @@
 import {HarvestingRecord} from "../../models/Harvest Models/RecordModels.js";
 import express from "express";
-import {WaterRecord} from "../../models/Inventory Models/waterModel.js";
+
+
 
 
 const router = express.Router();
@@ -42,19 +43,71 @@ router.post('/', async(request, response) =>
     }
 });
 
-router.get('/',async (request,response)=>
-{
-    try{
+//Route to get harvest records
+router.get('/', async (request, response) => {
+    try {
         const Records = await HarvestingRecord.find({});
         return response.status(200).json({
-            count: HarvestingRecord.length,
-            data: HarvestingRecord
+            count: Records.length, // Corrected to use Records.length
+            data: Records // Corrected to use Records
+        });
+    } catch (error) {
+        console.log(error.message);
+        response.status(500).json({ message: error.message }); // Corrected to use response.json()
+    }
+});
+
+//Route to get a harvest record by ID
+router.get('/:id', async (request, response) => {
+    try {
+        const { id } = request.params;
+
+        const record = await HarvestingRecord.findById(id);
+
+        if (!record) {
+            return response.status(404).json({ message: 'Record not found' });
+        }
+
+        return response.status(200).json(record);
+    } catch (error) {
+        console.log(error.message);
+        response.status(500).json({ message: error.message });
+    }
+});
+
+
+
+
+router.put('/:id', async (request, response) => {
+    try {
+        if(
+            !request.body.date ||
+            !request.body.cropType ||
+            !request.body.ageOfYield ||
+            !request.body.wayPicked ||
+            !request.body.quantity ||
+            !request.body.remarks
+        ){
+
+            return response.status(400).send({
+                message: 'Send all required fields: title, author, publishYear',
             });
-        }catch (error) {
+        }
+
+        const { id } = request.params;
+
+        const result = await HarvestingRecord.findByIdAndUpdate(id, request.body);
+
+        if (!result) {
+            return response.status(404).json({ message: 'Harvest record not found' });
+        }
+
+        return response.status(200).send({ message: 'Harvest record updated successfully' });
+    } catch (error) {
         console.log(error.message);
         response.status(500).send({ message: error.message });
     }
-})
+});
 
 
 export default router;
