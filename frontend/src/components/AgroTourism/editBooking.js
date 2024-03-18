7589c06daffc1e81602bebd1eaf1baac5449a041
IT22086334 <it22086334@my.sliit.lk>
2024-03-18 11:23:22 +0530

    colour

--- frontend/src/components/AgroTourism/editBooking.js
+++ frontend/src/components/AgroTourism/editBooking.js
@@ -195,7 +195,7 @@ const EditBooking = () => {
                     </button>
                     <button
                         type="submit"
-                        className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                        className="rounded-md bg-black px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-emerald-800 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
                     >
                         Save
                     </button>
