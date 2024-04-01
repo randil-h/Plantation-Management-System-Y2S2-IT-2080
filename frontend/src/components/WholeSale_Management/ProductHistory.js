44613b50a8bfce7bb48d0519089fa1fd226681c9
Vinuk Lakvindu <it22055026@my.sliit.lk>
2024-04-02 01:10:20 +0530

    search bar frontend

--- frontend/src/components/WholeSale_Management/ProductHistory.js
+++ frontend/src/components/WholeSale_Management/ProductHistory.js
@@ -35,6 +35,30 @@ const ProductHistory = () =>{
 
     return(
         <div>
+            <div className="flex justify-between items-center">
+                <div className="-mt-2 p-2 lg:mt-0 lg:w-full lg:max-w-md lg:flex-shrink-0">
+                    <div className="max-w-xs px-8">
+                        <a href="/OperationManager/AddingProduct"
+                           className="mt-10 block w-full rounded-md bg-indigo-600 px-3 py-2 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500
+                               focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"> Add
+                            a Product
+                        </a>
+                    </div>
+                </div>
+                <div className="mt-10">
+                    <div className="max-w-xs px-8">
+                        <input
+                            type="text"
+                            placeholder="Search Product"
+                            value={searchQuery}
+                            //onChange={handleSearch}
+                            className="border border-gray-300 rounded-full px-3 py-1"
+                        />
+                    </div>
+
+                </div>
+            </div>
+
             <div class="flex flex-col">
                 <div class="overflow-x-auto sm:mx-0.5 lg:mx-0.5">
                     <div class="py-2 inline-block min-w-full sm:px-6 lg:px-8">
--- frontend/src/pages/WholeSaleMangement/OperationManager/WholeSaleDashBoard.js
+++ frontend/src/pages/WholeSaleMangement/OperationManager/WholeSaleDashBoard.js
@@ -6,15 +6,15 @@ export default function wholeSaleDashBoard(){
     return(
         <div className="border-b sticky top-0 z-10">
             <Navbar/>
-            <div className="-mt-2 p-2 lg:mt-0 lg:w-full lg:max-w-md lg:flex-shrink-0">
-                <div className="mx-auto max-w-xs px-8">
-                    <a href="/OperationManager/AddingProduct"
-                       className="mt-10 block w-full rounded-md bg-indigo-600 px-3 py-2 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500
-                                       focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"> Add
-                        a Product
-                    </a>
-                </div>
-            </div>
+            {/*<div className="-mt-2 p-2 lg:mt-0 lg:w-full lg:max-w-md lg:flex-shrink-0">*/}
+            {/*    <div className="mx-auto max-w-xs px-8">*/}
+            {/*        <a href="/OperationManager/AddingProduct"*/}
+            {/*           className="mt-10 block w-full rounded-md bg-indigo-600 px-3 py-2 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500*/}
+            {/*                           focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"> Add*/}
+            {/*            a Product*/}
+            {/*        </a>*/}
+            {/*    </div>*/}
+            {/*</div>*/}
             <div>
                 <ProductHistory/>
             </div>
