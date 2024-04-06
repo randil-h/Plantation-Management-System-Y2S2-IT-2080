43e8fbdb0ed56551fafc4e1e579b4c5a52bf990d
Vinuk Lakvindu <it22055026@my.sliit.lk>
2024-04-06 14:18:21 +0530

    order backend

---
+++ backend/routes/Wholesale  Routes/OrderRoute.js
@@ -0,0 +1,48 @@
+import express, {request, response} from "express";
+import {Orders} from '../../models/Wholesale Models/OrderModel.js'
+
+const router = express.Router();
+
+router.post('/', async(request, response) =>{
+    try{
+        if(
+            !request.body.orderID ||
+            !request.body.orderDate ||
+            !request.body.orderQuantity ||
+            !request.body.orderPrice
+        ){
+            return response.status(400).send ({
+                message: 'Send all required fields',
+        });
+    }
+        const newOrder = {
+            orderID: request.body.orderID,
+            orderDate: request.body.orderDate,
+            productDescription: request.body.productDescription,
+            orderQuantity: request.body.orderQuantity,
+            orderPrice: request.body.orderPrice,
+        };
+
+        const order = await Orders.create(newOrder);
+
+        return response.status(201).send(order);
+    }catch (error){
+        console.log(error.message);
+        response.status(500).send({message:error.message});
+    }
+});
+
+router.get('/', async (request,response) =>{
+    try{
+        const orderrecords = await Orders.find({});
+        return response.status(200).json({
+            count: orderrecords.length,
+            data: orderrecords
+        });
+    }catch(error){
+        console.log(error.message);
+        response.status(500).send({ message: error.message });
+    }
+});
+
+export default router;
