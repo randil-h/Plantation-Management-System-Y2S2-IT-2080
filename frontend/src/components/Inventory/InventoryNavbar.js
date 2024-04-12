263ba2edb620ec19258d1734aefdde297411c7a3
Susadi11 <susadisandanima@gmail.com>
2024-04-12 19:48:29 +0530

    update inventory dashboard

--- frontend/src/components/Inventory/InventoryNavbar.js
+++ frontend/src/components/Inventory/InventoryNavbar.js
@@ -11,6 +11,12 @@ const menuItems = [
 export default function Example() {
     const [open, setOpen] = useState(false)
     const location = useLocation();
+    const [activeTab, setActiveTab] = useState(null);
+
+    const handleTabClick = (path) => {
+        setActiveTab(path);
+    };
+
     const isActive = (path) => {
         // Check if the current pathname starts with the specified path
         return location.pathname.startsWith(path);
@@ -27,8 +33,9 @@ export default function Example() {
                                     <li
                                         key={item.name}
                                         className={`flex focus:outline-none focus:ring focus:ring-lime-500 ${
-                                            isActive(item.path) ? " text-gray-100 px-2 py-0.5 bg-gradient-to-tr from-emerald-500 via-green-500 to-lime-400 rounded-full " : "hover:text-gray-600"
+                                            isActive(item.path) ? " text-gray-100 px-2 py-0.5 bg-gradient-to-tr from-emerald-500 via-green-500 to-lime-400 rounded-full " : "hover:bg-lime-200 rounded-full  hover:py-1 transition-all duration-200"
                                         }`}
+                                        onClick={() => handleTabClick(item.path)}
                                     >
                                         <Link to={item.path} className="px-2 flex items-center ">
                                             {item.icon && <item.icon className="mr-4" />}
