a0218d624ed285ca7ff7448a59dc7e7d9961a97e
Vinuk Lakvindu <it22055026@my.sliit.lk>
2024-04-18 09:08:03 +0530

    wertyui

--- frontend/src/components/WholeSale_Management/OrderHistory.js
+++ frontend/src/components/WholeSale_Management/OrderHistory.js
@@ -3,6 +3,7 @@ import { TbShoppingCartCopy } from "react-icons/tb";
 import {InformationCircleIcon, PencilSquareIcon, TrashIcon} from "@heroicons/react/24/outline";
 import axios from "axios";
 //import {useId} from 'react';
+import ProgressBar from "../../components/WholeSale_Management/ProgressBar"
 
 const OrderHistory = () => {
     const [orderRecords, setOrderRecords] = useState([]);
@@ -66,10 +67,10 @@ const OrderHistory = () => {
                                     className="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
                                     Price
                                 </th>
-                                <th
-                                    className="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
-                                    Status
-                                </th>
+                                {/*<th*/}
+                                {/*    className="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">*/}
+                                {/*    Status*/}
+                                {/*</th>*/}
                                 <th
                                     className="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
                                     Update
@@ -112,11 +113,11 @@ const OrderHistory = () => {
                                             {record.orderPrice}
                                         </p>
                                     </td>
-                                    <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
-                                        <p className="text-gray-900 whitespace-no-wrap">
-                                            Wrapping
-                                        </p>
-                                    </td>
+                                    {/*<td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">*/}
+                                    {/*    <p className="text-gray-900 whitespace-no-wrap">*/}
+                                    {/*        Wrapping*/}
+                                    {/*    </p>*/}
+                                    {/*</td>*/}
                                     {/*<td className=" px-5 py-5 border-b border-gray-200 bg-white text-sm items-center">*/}
                                     {/*    <a href="#"*/}
                                     {/*       className="font-medium text-blue-600  hover:underline">*/}
@@ -148,12 +149,13 @@ const OrderHistory = () => {
                                                 aria-hidden="true"/>
                                         </button>
                                     </td>
+                                    <div className="mt-32 items-center">
+                                        <ProgressBar/>
+                                    </div>
                                 </tr>
                             ))}
                             </tbody>
                         </table>
-
-
                     </div>
                 </div>
             </div>
