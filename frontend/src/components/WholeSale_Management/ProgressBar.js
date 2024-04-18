45d117b0fd4cd389adf73cdf24e4e0dec274dd1c
Vinuk Lakvindu <it22055026@my.sliit.lk>
2024-04-18 11:35:20 +0530

    mnkaoq

--- frontend/src/components/WholeSale_Management/ProgressBar.js
+++ frontend/src/components/WholeSale_Management/ProgressBar.js
@@ -2,12 +2,15 @@ import React from 'react';
 
 const ProgressBar = ({ progress }) => {
     return (
-        <div className="bg-gray-200 h-2 w-1/8 rounded-full">
-            <div
-                className="bg-blue-500 h-full rounded-full"
-                style={{ width: `${50}%` }}
-            ></div>
+        <div className="flex items-center justify-center mt-2 p-4">
+            <div className="bg-gray-200 h-2 rounded-full" style={{ width: 1250}}>
+                <div
+                    className="bg-blue-500 h-full rounded-full"
+                    style={{width: `${75}%`}}
+                ></div>
+            </div>
         </div>
+
     );
 };
 
