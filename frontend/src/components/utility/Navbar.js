56630bf09f0e06bfb747f0a75360a55033608968
Scheffel333 <118644309+Scheffel333@users.noreply.github.com>
2024-04-20 23:55:46 +0530

    navbar updates

--- frontend/src/components/utility/Navbar.js
+++ frontend/src/components/utility/Navbar.js
@@ -29,197 +29,55 @@ export default function Navbar() {
     const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
     const { login, register } = useKindeAuth();
 
+
+
     return (
-        <header className="bg-white bg-opacity-70  backdrop-blur text-emerald-950 sticky top-0 w-screen z-50 shadow-md">
+        <header className="bg-white bg-opacity-70 backdrop-blur text-emerald-950 sticky top-0 w-screen z-50 shadow-md">
             <nav
-                className="text-lg mx-auto flex max-w-7xl items-center relative justify-between p-6 lg:px-8 pt-2 pb-2 ">
+                className="text-lg h-full  mx-auto flex max-w-7xl items-center relative justify-between p-6 lg:px-8 py-2 gap-4">
                 <div className="flex lg:flex-1">
                     <a href="/" className="-m-1.5 p-1.5">
-                        <span className="text-2xl font-bold flex flex-row">ELEMAHANA <span className="font-light text-base">&trade;</span></span>
+                        <span className="text-2xl font-bold flex flex-row">ELEMAHANA <span
+                            className="font-light text-base">&trade;</span></span>
                     </a>
                 </div>
-
-                <div className="flex lg:hidden">
-                    <button
-                        type="button"
-                        className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
-                        onClick={() => setMobileMenuOpen(true)}
-                    >
-                        <span className="sr-only">Open main menu</span>
-                        <Bars3Icon className="h-6 w-6" aria-hidden="true"/>
-                    </button>
-                </div>
-                <Popover.Group className="hidden lg:flex lg:gap-x-12">
-                    <Popover className="relative">
-
-
-                        <Transition
-                            as={Fragment}
-                            enter="transition ease-out duration-200"
-                            enterFrom="opacity-0 translate-y-1"
-                            enterTo="opacity-100 translate-y-0"
-                            leave="transition ease-in duration-150"
-                            leaveFrom="opacity-100 translate-y-0"
-                            leaveTo="opacity-0 translate-y-1"
-                        >
-                            <Popover.Panel
-                                className="absolute -left-8 top-full z-10 mt-3 w-screen max-w-md overflow-hidden rounded-3xl bg-gray-100 shadow-lg ring-1 ring-gray-900/5">
-                                <div className="p-4">
-                                    {products.map((item) => (
-                                        <div
-                                            key={item.name}
-                                            className="group relative flex items-center gap-x-6 rounded-lg p-4 text-sm leading-6 hover:bg-lime-200"
-                                        >
-                                            <div
-                                                className="flex h-11 w-11 flex-none items-center justify-center rounded-lg bg-gray-50 group-hover:bg-white">
-                                                <item.icon className="h-6 w-6 text-gray-600 group-hover:text-lime-500"
-                                                           aria-hidden="true"/>
-                                            </div>
-                                            <div className="flex-auto">
-                                                <a href={item.href} className="block font-semibold text-gray-900">
-                                                    {item.name}
-                                                    <span className="absolute inset-0"/>
-                                                </a>
-                                                <p className="mt-1 text-gray-600">{item.description}</p>
-                                            </div>
-                                        </div>
-                                    ))}
-                                </div>
-                                <div className="grid grid-cols-2 divide-x divide-gray-900/5 bg-gray-200">
-                                    {callsToAction.map((item) => (
-                                        <a
-                                            key={item.name}
-                                            href={item.href}
-                                            className="flex items-center justify-center gap-x-2.5 p-3 text-sm font-semibold leading-6 text-gray-900 hover:bg-gray-100"
-                                        >
-                                            <item.icon className="h-5 w-5 flex-none text-gray-400" aria-hidden="true"/>
-                                            {item.name}
-                                        </a>
-                                    ))}
-                                </div>
-                            </Popover.Panel>
-                        </Transition>
-                    </Popover>
-
-                    <a href="/" className=" font-medium leading-6 hover:text-lime-600">
+                <a href="/" className="nav-item">
+                    <div
+                        className=" h-full font-medium px-6 rounded-full transition-all duration-200 hover:bg-lime-200">
                         Home
-                    </a>
 
-                    <a href="/placeOrder" className=" font-medium leading-6 hover:text-lime-600">
-
+                    </div>
+                </a>
+                <a href="/placeOrder" className="nav-item">
+                    <div className="h-full font-medium px-6 rounded-full transition-all duration-200 hover:bg-lime-200">
                         Marketplace
-                    </a>
-                    <a href="/tourism" className=" font-medium leading-6 hover:text-lime-600">
+
+                    </div>
+                </a>
+                <a href="/tourism" className="nav-item">
+                    <div className="h-full font-medium px-6 rounded-full transition-all duration-200 hover:bg-lime-200">
                         Visit Us
-                    </a>
 
-                    <a href="/dashboard" className=" font-medium leading-6 hover:text-lime-600">
+                    </div>
+                </a>
+                <a href="/dashboard" className="nav-item">
+                    <div className="h-full font-medium px-6 rounded-full transition-all duration-200 hover:bg-lime-200">
                         Dashboard
-                    </a>
 
-                </Popover.Group>
-                <div className="hidden lg:flex lg:flex-1 lg:justify-end gap-4 font-medium content-center items-center align-middle">
-                    <button onClick={register} type="button"
-                            className="border-black border px-4 rounded-full hover:border-lime-600 hover:text-lime-600">Register
-                    </button>
-                    |
-                    <button onClick={login} type="button" className="hover:text-lime-600">Log In</button>
-                    <UserCircleIcon className="w-6 h-6"/>
-                </div>
+            </div></a>
 
-            </nav>
-            <Dialog as="div" className="lg:hidden" open={mobileMenuOpen} onClose={setMobileMenuOpen}>
-                <div className="fixed inset-0 z-10"/>
-                <Dialog.Panel
-                    className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
-                    <div className="flex items-center justify-between">
-                        <a href="#" className="-m-1.5 p-1.5">
-                            <span className="sr-only">Your Company</span>
-                            <img
-                                className="h-8 w-auto"
-                                src="../../../public/elemahana_logo.png"
-                                alt=""
-                            />
-                        </a>
-                        <button
-                            type="button"
-                            className="-m-2.5 rounded-md p-2.5 text-gray-700"
-                            onClick={() => setMobileMenuOpen(false)}
-                        >
-                            <span className="sr-only">Close menu</span>
-                            <XMarkIcon className="h-6 w-6" aria-hidden="true"/>
-                        </button>
-                    </div>
-                    <div className="mt-6 flow-root">
-                        <div className="-my-6 divide-y divide-gray-500/10">
-                            <div className="space-y-2 py-6">
-                                <Disclosure as="div" className="-mx-3">
-                                    {({open}) => (
-                                        <>
-                                            <Disclosure.Button
-                                                className="flex w-full items-center justify-between rounded-lg py-2 pl-3 pr-3.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">
-                                                Product
-                                                <ChevronDownIcon
-                                                    className={classNames(open ? 'rotate-180' : '', 'h-5 w-5 flex-none')}
-                                                    aria-hidden="true"
-                                                />
-                                            </Disclosure.Button>
-                                            <Disclosure.Panel className="mt-2 space-y-2">
-                                                {[...products, ...callsToAction].map((item) => (
-                                                    <Disclosure.Button
-                                                        key={item.name}
-                                                        as="a"
-                                                        href={item.href}
-                                                        className="block rounded-lg py-2 pl-6 pr-3 text-sm font-semibold leading-7 text-gray-900 hover:bg-gray-50"
-                                                    >
-                                                        {item.name}
-                                                    </Disclosure.Button>
-                                                ))}
-                                            </Disclosure.Panel>
-                                        </>
-                                    )}
-                                </Disclosure>
-                                <a
-                                    href="#"
-                                    className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
-                                >
-                                    Features
-                                </a>
-                                <a
-                                    href="/placeOrder"
-                                    className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
-                                >
-                                    Marketplace
-                                </a>
+            <div
+                className="hidden lg:flex lg:flex-1 lg:justify-end gap-4 font-medium content-center items-center align-middle">
+                <button onClick={register} type="button"
+                        className="border-black border px-4 rounded-full hover:border-lime-600 hover:text-lime-600">Register
+                </button>
+                |
+                <button onClick={login} type="button" className="hover:text-lime-600">Log In</button>
+                <UserCircleIcon className="w-6 h-6"/>
+            </div>
+        </nav>
+</header>
 
-                                <a
-                                    href="/booking"
-                                    className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
-                                >
-                                    Visit Us
-                                </a>
 
-                                <a
-                                    href="/src/pages/Dashboard"
-                                    className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
-                                >
-                                    Dashboard
-                                </a>
-
-                            </div>
-                            <div className="py-6">
-                                <a
-                                    href="#"
-                                    className="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
-                                >
-                                Log in
-                                </a>
-                            </div>
-                        </div>
-                    </div>
-                </Dialog.Panel>
-            </Dialog>
-        </header>
-
-    )
+)
 }
