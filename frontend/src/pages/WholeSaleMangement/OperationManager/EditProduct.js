f8850e52f59b5513c979cf19e64f866729b29ea8
Vinuk Lakvindu <it22055026@my.sliit.lk>
2024-05-04 10:47:43 +0530

    chng

--- frontend/src/pages/WholeSaleMangement/OperationManager/EditProduct.js
+++ frontend/src/pages/WholeSaleMangement/OperationManager/EditProduct.js
@@ -1,14 +1,37 @@
 import React from "react";
 import Navbar from "../../../components/utility/Navbar";
 import ProductEdtingForm from "../../../components/WholeSale_Management/ProductEditingForm";
+import SideBar from "../../../components/SideBar";
+import {SnackbarProvider} from "notistack";
+import BackButton from "../../../components/utility/BackButton";
+import Breadcrumb from "../../../components/utility/Breadcrumbs";
 
 export default function EditProduct(){
+
+    const breadcrumbItems = [
+        { name: 'Products', href: '/wholesaleDashboard' },
+        { name: 'Edit Product', href: '/editProduct/:id' },
+    ];
+
     return(
-        <div>
-            <Navbar/>
-            <div>
-                <ProductEdtingForm/>
+        <SnackbarProvider>
+            <div className="sticky top-0 z-10">
+                <Navbar/>
             </div>
-        </div>
+            <div className="grid sm:grid-cols-6 ">
+                <div className="  col-span-1 sticky top-0">
+                    <SideBar/>
+                </div>
+                <div className="w-full col-span-5 flex flex-col ">
+                    <div className="flex flex-row  ">
+                        <BackButton/>
+                        <Breadcrumb items={breadcrumbItems}/>
+
+                    </div>
+                    <ProductEdtingForm/>
+                </div>
+            </div>
+        </SnackbarProvider>
+
     );
 }
